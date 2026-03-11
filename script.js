f2280d0ad99f931d5b8299fc00c711e7149fddeb
RENTAGONE <rentagone.social@gmail.com>
2026-03-11 09:37:17 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,45 +1,87 @@
-// ROBUX CALCULATOR
-const robuxInput = document.getElementById("robux");
-const priceSpan = document.getElementById("price");
+// PRICE RATE
 
-const rate = 75 / 100; // 100 robux = 75 BDT
+const rate = 0.75
 
-if (robuxInput) {
-robuxInput.addEventListener("input", () => {
-let robux = parseFloat(robuxInput.value) || 0;
-let price = robux * rate;
-priceSpan.textContent = price.toFixed(2);
-});
+
+// PARSE ROBUX INPUT
+
+function parseRobux(input){
+
+input = input.toLowerCase()
+
+if(input.includes("k")) return parseFloat(input)*1000
+if(input.includes("m")) return parseFloat(input)*1000000
+if(input.includes("b")) return parseFloat(input)*1000000000
+if(input.includes("t")) return parseFloat(input)*1000000000000
+
+return parseFloat(input)
+
 }
 
 
-// SEND ORDER TO DISCORD
+// CALCULATOR
+
+const robuxInput = document.getElementById("robuxInput")
+const priceDisplay = document.getElementById("price")
+
+if(robuxInput){
+
+robuxInput.addEventListener("input",()=>{
+
+let robux = parseRobux(robuxInput.value) || 0
+
+let price = robux * rate
+
+priceDisplay.textContent = price.toFixed(2)
+
+})
+
+}
+
+
+
+// ORDER SYSTEM
+
 async function sendOrder(){
 
-const username = document.getElementById("username").value;
-const discord = document.getElementById("discord").value;
-const robux = document.getElementById("orderRobux").value;
-const payment = document.getElementById("payment").value;
+const siteUser = document.getElementById("siteUser").value
+const robloxUser = document.getElementById("robloxUser").value
+const discordUser = document.getElementById("discordUser").value
+const robux = document.getElementById("orderAmount").value
+const payment = document.getElementById("paymentMethod").value
 
-const webhook = "https://discord.com/api/webhooks/1481295676041072742/ouT3Fc0B-flRl_9BzTqDCNfdPvcY59GGvgd4PqHMTNB-2aEf81qYZGbpRXMcDsUYe57l";
+const webhook = "https://discord.com/api/webhooks/1481295676041072742/ouT3Fc0B-flRl_9BzTqDCNfdPvcY59GGvgd4PqHMTNB-2aEf81qYZGbpRXMcDsUYe57l"
 
 const message = {
+
 content:
-`📦 NEW RENTAGONE ORDER
+`NEW RENTAGONE ORDER
 
-👤 Username: ${username}
-💬 Discord: ${discord}
-💰 Robux Amount: ${robux}
-💳 Payment Method: ${payment}`
-};
+Site User: ${siteUser}
 
-await fetch(webhook,{
+Roblox User: ${robloxUser}
+
+Discord: ${discordUser}
+
+Robux Amount: ${robux}
+
+Payment: ${payment}
+
+Status: Pending`
+}
+
+fetch(webhook,{
+
 method:"POST",
+
 headers:{
 "Content-Type":"application/json"
 },
-body:JSON.stringify(message)
-});
 
-alert("Order submitted! Check Discord.");
+body:JSON.stringify(message)
+
+})
+
+alert("Order Sent")
+
 }
